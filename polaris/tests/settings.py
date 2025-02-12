3bd670f22c07149cfe4cebfcb2d34e427dcc4f50
Jun Luo <4catcode@gmail.com>
2025-02-12 15:24:56 +0800

    bump deps

--- polaris/tests/settings.py
+++ polaris/tests/settings.py
@@ -1,6 +1,11 @@
 from settings import *
 from polaris import settings
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 settings.LOCAL_MODE = False
 SESSION_COOKIE_SECURE = True
 INSTALLED_APPS.remove("server")
